c849b468a20d1558aba212ea6140ca496ea8557c
divyansh0548 <87368053+divyansh0548@users.noreply.github.com>
2021-07-13 16:10:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var box;
+function setup() {
+  createCanvas(400, 400);
+  box = createSprite(200, 200, 10, 10)
+}
+
+function draw() {
+  background(30);
+  if (keyIsDown(LEFT_ARROW)) {
+    box.x = box.x - 2
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    box.x = box.x + 2
+  }
+  drawSprites()
+}
+
+
+
+
